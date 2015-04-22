4cd5cbf5124b5891b51313b47824d7f08b2ae871
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-04-22 12:20:26 -0400

    Changed docker MONGO variable

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 # Configure MongoDB
-sed s/localhost:27017/$MONGODB_PORT_28017_TCP_ADDR:$MONGODB_PORT_27017_TCP_PORT/g /etc/mica2/application.yml > /tmp/application.yml
+sed s/localhost:27017/$MONGO_PORT_28017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/g /etc/mica2/application.yml > /tmp/application.yml
 mv -f /tmp/application.yml /etc/mica2/application.yml
 # Configure Opal
 sed s/localhost:8443/$OPAL_PORT_8443_TCP_ADDR:$OPAL_PORT_8443_TCP_PORT/g /etc/mica2/application.yml > /tmp/application.yml
